4c413cb4e6ea84cd305f84d1315b8264fac218a1
Sirius018 <betochirakudai@gmail.com>
2023-06-07 16:46:09 -0500

    avance index

--- ADW/InventoryGenius/src/main/java/com/configuration/WebMvcConfig.java
+++ ADW/InventoryGenius/src/main/java/com/configuration/WebMvcConfig.java
@@ -5,10 +5,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfig implements WebMvcConfigurer {
 	
+	/*
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
-    }
+    }*/
 
 }
+ 
