badf168a8ecc86d2426b6eeea88586ac566d3cf6
Sirius018 <betochirakudai@gmail.com>
2023-06-07 23:47:22 -0500

    :1

--- ADW/InventoryGenius/src/main/java/com/inventorygenius/InventoryGeniusApplication.java
+++ ADW/InventoryGenius/src/main/java/com/inventorygenius/InventoryGeniusApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryGeniusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryGeniusApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(InventoryGeniusApplication.class, args); }
+
+
+
 
 }
